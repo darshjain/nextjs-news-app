6044d3b6d515d226f736d5c38d3ffb87f1a6f619
darshjain <darshj786@gmail.com>
2021-09-11 09:06:40 +0530

    Feed Page Ready

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,11 +1,51 @@
+import { Toolbar } from '../../components/toolbar'
+import styles from '../../styles/Feed.module.css'
+import { useRouter } from 'next/router'
+
 export const Feed = ({ pageNumber, articles }) => {
+  const router = useRouter()
   return (
     <div className="page-container">
-      {articles.map((article, index) => (
-        <div key={index} className={styles.post}>
-          <h1>{article.title}</h1>
+      <Toolbar />
+      <div className={styles.main}>
+        {articles.map((article, index) => (
+          <div key={index} className={styles.post}>
+            <h1 onClick={() => (window.location.href = article.url)}>
+              {article.title}
+            </h1>
+            <p>{article.description}</p>
+            {!!article.urlToImage && <img src={article.urlToImage} />}
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.paginator}>
+        <div
+          onClick={() => {
+            if (pageNumber > 1) {
+              router
+                .push('/feed/${pageNumber-1}')
+                .then(() => window.scrollTo(0, 0))
+            }
+          }}
+          className={pageNumber === 1 ? styles.disabled : styles.active}
+        >
+          Previous Page
         </div>
-      ))}
+        <div>#{pageNumber}</div>
+        <div
+          onClick={() => {
+            if (pageNumber < 5) {
+              router
+                .push('/feed/${pageNumber+1}')
+                .then(() => window.scrollTo(0, 0))
+            }
+          }}
+          className={pageNumber === 5 ? styles.disabled : styles.active}
+        >
+          Next Page
+        </div>
+      </div>
     </div>
   )
 }
@@ -21,23 +61,24 @@ export const getServerSideProps = async (pageContext) => {
       },
     }
   }
+
   const apiResponse = await fetch(
-    'https://newsapi.org/v2/top-headlines?country=us&pageSize=5&page=${pageNumber}',
+    `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&page=${pageNumber}`,
     {
       headers: {
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
       },
     },
-  )
-  const apiJson = await apiResponse.json()
+  ).then((res) => res.json())
 
-  const { articles } = apiJson
+  const { articles } = apiResponse
 
   return {
     props: {
+      articles: articles,
       pageNumber: Number.parseInt(pageNumber),
-      articles,
     },
   }
 }
+
 export default Feed
